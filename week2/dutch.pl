ba76c6ede877bc03733cb9460b859fe892a62e06
Lang Martin <lmartin@opentable.com>
2016-02-16 12:21:06 -0500

    week2 post discussion

--- week2/dutch.pl
+++ week2/dutch.pl
@@ -17,3 +17,19 @@ dutch([], [], [], []).
 dutch(Rs, Ws, Bs, [X|Xs]) :- X = red,   dutch([X|Rs], Ws, Bs, Xs).
 dutch(Rs, Ws, Bs, [X|Xs]) :- X = white, dutch(Rs, [X|Ws], Bs, Xs).
 dutch(Rs, Ws, Bs, [X|Xs]) :- X = blue,  dutch(Rs, Ws, [X|Bs], Xs).
+
+%% dutch2, after looking at Brent's
+
+red(r).
+white(w).
+blue(b).
+
+ndp([], [], [], []).
+ndp([X|Rs], Ws, Bs, [X|Xs]) :- red(X),   ndp(Rs, Ws, Bs, Xs).
+ndp(Rs, [X|Ws], Bs, [X|Xs]) :- white(X), ndp(Rs, Ws, Bs, Xs).
+ndp(Rs, Ws, [X|Bs], [X|Xs]) :- blue(X),  ndp(Rs, Ws, Bs, Xs).
+
+nd(Xs, Ps) :-
+    ndp(Rs, Ws, Bs, Xs),
+    append(Rs, Ws, Qs),
+    append(Qs, Bs, Ps).
--- week2/repl.pl
+++ week2/repl.pl
@@ -7,3 +7,8 @@ dutch([white], [blue], [white], [white, blue, red]).
 dutch([red], [blue], [white], []).
 
 dutch([red], [blue], [white], [white, blue, red]).
+
+nd([r, b, w, w, w, r, b, w], [r, r, w, w, w, w, b, b]).
+
+%% never returns
+%% nd(Zs, [r, r, w, w, w, w, b, b]).
